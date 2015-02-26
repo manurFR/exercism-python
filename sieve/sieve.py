2f02b5e901f0e7824b74d7626fbc74632136b12d
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2015-02-26 13:57:10 +0100

    optimization for sieve

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -3,8 +3,8 @@ def sieve(limit):
     current_index = 0
     while current_index < len(primes):
         current = primes[current_index]
-        for factor in range(2, (limit/current)+1):
-            if current * factor in primes:
-                primes.remove(current * factor)
+        for composite in range(current*current, limit+1, current):
+            if composite in primes:
+                primes.remove(composite)
         current_index += 1
     return primes
