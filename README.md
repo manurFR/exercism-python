d901bd0d314c9ac5da93db59de454b23d3c5b2fa
Emmanuel Bizieau <emmanuel.bizieau@laposte.net>
2015-03-03 16:56:55 +0100

    luhn

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ My python submissions to the exercism.io pratice problems.
 - [allergies](allergies/allergies.py)
 - [atbash cipher](atbash-cipher/atbash_cipher.py)
 - [kindergarten garden](kindergarten-garden/garden.py)
+- [luhn] (luhn/luhn.py)
 - [meetup](meetup/meetup.py)
 - [series](series/series.py) and [largest series product](largest-series-product/largest_series_product.py)
 - [space age](space-age/space_age.py)
--- allergies/allergies.py
+++ allergies/allergies.py
@@ -3,7 +3,7 @@ ALLERGENS = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocol
 
 class Allergies(object):
     def __init__(self, score):
-        self.list = [allergen for exponant, allergen in enumerate(ALLERGENS) if score & 2**exponant > 0]
+        self.list = [allergen for exponant, allergen in enumerate(ALLERGENS) if score & 2 ** exponant > 0]
 
     def is_allergic_to(self, allergen):
         return allergen.lower() in self.list
